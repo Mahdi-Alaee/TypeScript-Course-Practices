0288e7f3be1efa8b8168df781791e992d9bb5415
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-04 18:39:55 +0330

    practice 1: coding plus function

--- practice1_first-project/app.js
+++ practice1_first-project/app.js
@@ -1,1 +1,6 @@
-console.log('hello ts');
+function plus(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    return "can't plus with one number";
+}
+console.log(plus(5, 5));
--- practice1_first-project/app.ts
+++ practice1_first-project/app.ts
@@ -1,1 +1,6 @@
-console.log('hello ts');
+function plus(num1: number, num2?: number) {
+  if (num2) return num1 + num2;
+  return "can't plus with one number";
+}
+
+console.log(plus(5, 5));
