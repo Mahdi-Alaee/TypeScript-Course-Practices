a66f53622ddf4c453758737a3903eabb65a48f78
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-05 19:14:03 +0330

    assign types to an Object

---
+++ practice2_deeper-in-core-types/app.ts
@@ -0,0 +1,29 @@
+//* assign type to an Object *//
+
+//! way 1
+// const person: object = {
+//   name: "mahdi",
+//   age: 17,
+// };
+
+//! way 2
+// const person: {} = {
+//   name: "mahdi",
+//   age: 17,
+// };
+
+//! best way
+// let person: {
+//     name: string;
+//     age: number;
+// };
+
+// person = {
+//     name: 'mahdi alaee',
+//     age: 17
+// };
+
+// console.log(person);
+
+//* assign type to an Array *//
+
