f9ba32e706d2536c8b8afbb09d9638eda1ebb3e5
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-05 17:49:07 +0330

    practice 1: remove some feature from project

--- practice1_first-project/app.ts
+++ practice1_first-project/app.ts
@@ -1,6 +1,5 @@
-function plus(num1: number, num2?: number) {
-  if (num2) return num1 + num2;
-  return "can't plus with one number";
+function plus(num1: number, num2: number) {
+  return num1 + num2;
 }
 
 console.log(plus(5, 5));
