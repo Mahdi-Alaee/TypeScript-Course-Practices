ef60e9a228902529d50867c97216fbdab0827c46
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-05 18:26:42 +0330

    practice 1: modifyed by type-inference

--- practice1_first-project/app.ts
+++ practice1_first-project/app.ts
@@ -1,11 +1,11 @@
 function plus(num1: number, num2: number, showResult: boolean, phrase: string) {
-  const result = num1 + num2;
+  const result: number = num1 + num2;
   if (showResult) 
     console.log(phrase + result);
   else 
     return num1 + num2;
 }
 
-const showResult = true;
+const showResult: boolean = true;
 
 console.log(plus(5, 5, showResult, 'result: '));
