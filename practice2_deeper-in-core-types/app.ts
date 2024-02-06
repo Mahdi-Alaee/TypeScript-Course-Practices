2be58414b7352f0eeb0dac64a3fbbc8b66b585f6
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-06 17:32:53 +0330

    learning new type whose name is "enum"

--- practice2_deeper-in-core-types/app.ts
+++ practice2_deeper-in-core-types/app.ts
@@ -38,9 +38,31 @@
 
 //* new type whose name is tuples *//
 
-type person = [string, number, string[]?];
+// type person = [string, number, string[]?];
 
-let testArray1: person[] = [
-  ['mahdi', 17, ['sports', 'jerk off']],
-  ['homayoon', 18, ['listen to musics', 'fucking fancy women']]
+// let testArray1: person[] = [
+//   ['mahdi', 17, ['sports', 'jerk off']],
+//   ['homayoon', 18, ['listen to musics', 'fucking fancy women']]
+// ];
+
+//* new type whose name is enum *//
+
+enum Role {
+  ADMIN = "ADMIN",
+  READ_ONLY = "READ_ONLY",
+  AUTHOR = "AUTHOR",
+}
+
+let article: { title: string; desc: string; author: string }[];
+
+article = [
+  {
+    title: "js-vs-java",
+    desc: "let`s check the deferent between js and java",
+    author: Role.ADMIN,
+  },
 ];
+
+if(article[0].author === Role.ADMIN){
+  console.log('this article is created by admin');
+}
