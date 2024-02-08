665a89befde7213c9ce7f42371d9756590833235
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-08 09:50:30 +0330

    practice 3 : noUnusedLocals & noUnusedParameters & noImplicitReturns configs

--- practice3_compiler-and-configs/src/app.ts
+++ practice3_compiler-and-configs/src/app.ts
@@ -1,6 +1,15 @@
-const btn = document.querySelector("button");
+// const btn = document.querySelector("button");
 
 // a comment which not compiled beacuse removeComments is "true"
-btn.addEventListener("click", () => {
-  console.log("clicked!");
-});
+// btn.addEventListener("click", () => {
+//   console.log("clicked!");
+// });
+
+let mahdi: Object;
+
+function add(num1: number, num2: number, ageInput: number) {
+  let age: number;
+  if (num1 > num2)
+   return num1 + num2;
+}
+
