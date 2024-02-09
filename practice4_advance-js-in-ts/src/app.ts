6c9347e5192c081280769072d33ee26fb54d7cc3
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-09 16:18:01 +0330

    Objects and Arrays Destructuring

--- practice4_advance-js-in-ts/src/app.ts
+++ practice4_advance-js-in-ts/src/app.ts
@@ -1,9 +1,27 @@
 //* Rest Parameters *//
 
-const add = (...args: number[]) => {
-  return args.reduce((prev: number, curr: number) => {
-    return prev + curr;
-  }, 0);
+// const add = (...args: number[]) => {
+//   return args.reduce((prev: number, curr: number) => {
+//     return prev + curr;
+//   }, 0);
+// };
+
+// console.log(add(12, 2, 4, 2));
+
+//* Objects and Arrays Destructuring */
+
+const numbers = [1,2,3,4];
+
+const [num1, num2, ...remainingNumbers] = numbers;
+
+console.log(num1, num2, remainingNumbers);
+
+
+const person = {
+    username: 'mahdi',
+    age: 17
 };
 
-console.log(add(12, 2, 4, 2));
+const {username: firstName, age} = person;
+
+console.log(firstName, age);
