bc192372b98c57932b0810bfc20300ded78b09b8
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-09 16:06:43 +0330

    Reducer and Rest Parameters

--- practice4_advance-js-in-ts/src/app.ts
+++ practice4_advance-js-in-ts/src/app.ts
@@ -1,2 +1,9 @@
 //* Rest Parameters *//
 
+const add = (...args: number[]) => {
+  return args.reduce((prev: number, curr: number) => {
+    return prev + curr;
+  }, 0);
+};
+
+console.log(add(12, 2, 4, 2));
