4fcbe3e90e3e1ad558dfa46f543bc5d10f0c33f8
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-11 11:34:02 +0330

    project 5 : inheritence

--- practice5_classes-interfaces/src/app.ts
+++ practice5_classes-interfaces/src/app.ts
@@ -17,22 +17,77 @@ class Department {
   }
 
   getEmployees() {
-    return this.employees;
-  }
-
-  changeIdValue(newValue: string){
-    this.id = newValue; //! can't do It because of "readonly" attribute
+    return 'employees: ' + this.employees;
   }
 }
 
-const accounting = new Department("1", "accounting");
+class ITDepartment extends Department {
+  admins: string[] = [];
+
+  constructor(id: string) {
+    super(id, "ITDepartment");
+  }
+
+  addAdmin(name: string) {
+    this.admins.push(name);
+  }
+
+  printAdmins() {
+    console.log("admins: " +  this.admins);
+  }
+}
+
+class AccountingDepartment extends ITDepartment {
+  reports: string[] = [];
+
+  constructor(id: string) {
+    super(id);
+  }
+
+  addReport(content: string) {
+    this.reports.push(content);
+  }
+
+  printReports() {
+    console.log('reports: ' + this.reports);
+  }
+}
+
+//! ITDepartment !//
+
+const it = new ITDepartment("IT1");
+
+it.describe();
+
+it.addEmployee("Ali");
+it.addEmployee("Hasan");
+it.addEmployee("Saman");
+it.addEmployee("Mahdi");
+
+it.addAdmin("mostafa");
+it.addAdmin("bagher");
+
+console.log(it.getEmployees());
+it.printAdmins();
+
+console.log(it);
+
+console.log("------------------------------------------------");
+
+//! AccountingDepartment !//
+
+const accounting = new AccountingDepartment("accounting1");
 
 accounting.describe();
-accounting.changeIdValue('2');
-accounting.describe();
-accounting.addEmployee("Ali");
-accounting.addEmployee("Hasan");
-accounting.addEmployee("Saman");
-accounting.addEmployee("Mahdi");
 
+accounting.addAdmin("mahdi");
+accounting.addEmployee("asghar");
+accounting.addReport("something went wrong1");
+accounting.addReport("something went wrong2");
+
+accounting.printAdmins();
 console.log(accounting.getEmployees());
+accounting.printReports();
+
+console.log(accounting);
+
