91e98e177ee8fa3b6dfa16c8c4c4ab139572113e
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-12 15:55:20 +0330

    readonly can be used in types and interfaces

--- practice5_classes-interfaces/src/app.ts
+++ practice5_classes-interfaces/src/app.ts
@@ -1,16 +1,18 @@
 interface greetable {
-  name: string;
+  readonly name: string;
 
   greet(phrase: string): void;
 }
 
 class Person implements greetable {
-  age: number = 17;
+  readonly age: number = 17;
 
   constructor(public name: string) {}
 
   greet(phrase: string): void {
     console.log(phrase + " " + this.name);
+    this.name = 'ali'; //! can do this
+    this.age = 12; //! but can't do this
   }
 }
 
@@ -19,4 +21,6 @@ let user1: greetable;
 user1 = new Person("mahdi");
 
 console.log(user1);
+user1.greet("yo I am -");
+// user1.name = 'mmd'; //! can't do this because of readonly property
 user1.greet("yo I am -");
