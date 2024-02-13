c75af1dbee61f937f631f233e64173d80c79e3a0
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-13 18:02:39 +0330

    project 6 : type gaurds

--- practice6_advances-of-TS/src/app.ts
+++ practice6_advances-of-TS/src/app.ts
@@ -1,5 +1,6 @@
 //* intersection types *//
 
+//! example 1
 type Admin = {
   name: string;
   departments: string[];
@@ -10,26 +11,71 @@ type Employee = {
   functions: string[];
 };
 
-type ElevatedEmployee = Admin & Employee;
+// type ElevatedEmployee = Admin & Employee;
 
-let person1: ElevatedEmployee;
+// let person1: ElevatedEmployee;
 
-person1 = {
-  name: "mahdi",
-  departments: ["courses", "managment"],
-  functions: ["developer", "team-lead"],
-};
+// person1 = {
+//   name: "mahdi",
+//   departments: ["courses", "managment"],
+//   functions: ["developer", "team-lead"],
+// };
 
-type StrAndNum = string | number;
-type NumAndBool = number | boolean
+//! example 2
 
-type StrAndNumAndBool = StrAndNum | NumAndBool;
+// type StrAndNum = string | number;
+// type NumAndBool = number | boolean
 
-let variable1: StrAndNumAndBool;
-variable1 = 12;
+// type StrAndNumAndBool = StrAndNum | NumAndBool;
 
-let variable2: StrAndNumAndBool;
-variable2 = 'test';
+// let variable1: StrAndNumAndBool;
+// variable1 = 12;
 
-let variable3: StrAndNumAndBool;
-variable3 = false;
+// let variable2: StrAndNumAndBool;
+// variable2 = 'test';
+
+// let variable3: StrAndNumAndBool;
+// variable3 = false;
+
+//* type gaurds *//
+
+//! example 1
+type UnknownEmployee = Admin | Employee;
+
+function employeeLogger(emp: UnknownEmployee) {
+  if ("departments" in emp) console.log(emp.departments);
+  if ("functions" in emp) console.log(emp.functions);
+}
+
+employeeLogger({
+  name: "soheil",
+  departments: ["courses", "support"],
+  functions: ["supporter"],
+});
+
+//! example 2
+class Car {
+  drive() {
+    console.log("car is driving ...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("truck is driving ...");
+  }
+
+  setCargo(amount: number) {
+    console.log(`${amount} cargo seted`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+function vehicleHandler(vehicle: Vehicle) {
+  if (vehicle instanceof Truck) vehicle.setCargo(12);
+  vehicle.drive();
+}
+
+vehicleHandler(new Car());
+vehicleHandler(new Truck());
