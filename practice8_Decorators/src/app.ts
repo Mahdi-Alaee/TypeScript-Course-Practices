b3754f0cf5cecca41c31eeca23d86b0918edb618
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-15 18:26:08 +0330

    project 8 : Coding WithTemplate Function

--- practice8_Decorators/src/app.ts
+++ practice8_Decorators/src/app.ts
@@ -1,20 +1,47 @@
 //* Decorator Class *//
 
 function Logger(logtext: string) {
-    return (constructor: Function) => {
-        console.log(logtext);
-        console.log(constructor);
-    }
+  return (constructor: Function) => {
+    console.log(logtext);
+    console.log(constructor);
+  };
 }
 
-@Logger('Person Class - Decorator')
+//! WithTemplate 1
+// function WithTemplate(hookId: string, template: string) {
+//   const hookElem = document.getElementById(hookId);
+
+//   return (_: any) => {
+//     if (hookElem) {
+//       hookElem.innerHTML = template;
+//     }
+//   };
+// }
+
+//! WithTemplate 2
+function WithTemplate(hookId: string, template: string) {
+  const hookElem = document.getElementById(hookId);
+
+  return (constructor: any) => {
+    if (hookElem) {
+      const instance = new constructor();
+      hookElem.innerHTML = `<h1>${instance.name}</h1>`;
+    }
+  };
+}
+
+// @Logger('Person Class - Decorator')
+@WithTemplate(
+  "app",
+  "<h1 style='color: white; background-color: black'>Hello! I'm Person Class WithTemplate Decorator</h1>"
+)
 class Person {
   name: string;
 
-  constructor(name: string) {
+  constructor(name: string = "mahdi") {
     console.log(`${name} added ...`);
     this.name = name;
   }
 }
 
-const user1 = new Person("mahdi");
+const user1 = new Person("Ali");
