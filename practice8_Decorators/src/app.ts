e4011b148d7738e878e2a028d701f8ca0cb2e0bc
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-15 17:30:14 +0330

    project 8 : Create the first Decorator Class

--- practice8_Decorators/src/app.ts
+++ practice8_Decorators/src/app.ts
@@ -1,1 +1,18 @@
-console.log('project started');
+//* Decorator Class *//
+
+function Logger(con: Function) {
+  console.log("logging ...");
+  console.log(con);
+}
+
+@Logger
+class Person {
+  name: string;
+
+  constructor(name: string) {
+    console.log(`${name} added ...`);
+    this.name = name;
+  }
+}
+
+const user1 = new Person("mahdi");
