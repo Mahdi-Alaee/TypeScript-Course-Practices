6b29935511bf9841fe3141d45e0d81d95e86b923
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-24 17:37:49 +0330

    project 10 : separate project all elements of project by namespace

---
+++ practice10_Spread-to-multiple-files/src/components/project-item.ts
@@ -0,0 +1,41 @@
+namespace App {
+  //* Project Item *//
+  export class ProjectItem
+    extends RenderableClass<HTMLUListElement, HTMLLIElement>
+    implements Draggable
+  {
+    constructor(rootElemId: string, private project: Project) {
+      super("single-project", rootElemId, "beforeend", project.id);
+
+      this.configure();
+      this.generateContent();
+    }
+
+    get peopleText() {
+      const peopleCount = this.project.people;
+      return `${peopleCount} ${
+        peopleCount > 1 ? "persons" : "person"
+      } assigned`;
+    }
+
+    @AutoBind
+    dragStartHandler(event: DragEvent) {
+      event.dataTransfer!.setData("text/plain", this.project.id);
+      event.dataTransfer!.effectAllowed = "move";
+    }
+
+    @AutoBind
+    dragEndHandler(_: DragEvent) {}
+
+    configure() {
+      this.element.addEventListener("dragstart", this.dragStartHandler);
+      this.element.addEventListener("dragend", this.dragEndHandler);
+    }
+
+    generateContent() {
+      this.element.querySelector("h2")!.innerHTML = this.project.title;
+      this.element.querySelector("h3")!.innerHTML = this.peopleText;
+      this.element.querySelector("p")!.innerHTML = this.project.description;
+    }
+  }
+}
---
+++ practice10_Spread-to-multiple-files/src/states/projects-state.ts
@@ -0,0 +1,55 @@
+namespace App {
+  //* Project State managment *//
+  export class ProjectState {
+    private projects: Project[] = [];
+    private listeners: Function[] = [];
+    private static instence: ProjectState;
+
+    private constructor() {}
+
+    moveProject(id: string) {
+      this.projects.forEach((project) => {
+        if (project.id === id) {
+          const newStatus =
+            project.status === ProjectStatus.Active
+              ? ProjectStatus.Finished
+              : ProjectStatus.Active;
+          project.status = newStatus;
+        }
+      });
+
+      this.updateListeners();
+    }
+
+    updateListeners() {
+      for (let listenerFn of this.listeners) {
+        listenerFn(this.projects);
+      }
+    }
+
+    static getInstence() {
+      if (this.instence) return this.instence;
+
+      return new ProjectState();
+    }
+
+    addListener(listenerFn: Function) {
+      this.listeners.push(listenerFn);
+    }
+
+    addProject(title: string, description: string, people: number) {
+      const newProject: Project = {
+        id: crypto.randomUUID(),
+        title,
+        description,
+        people,
+        status: 0,
+      };
+
+      this.projects.push(newProject);
+      this.updateListeners();
+    }
+  }
+
+  export const prjState = ProjectState.getInstence();
+}
