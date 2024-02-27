2bbcd5a272a255a4c2c38dd3d13567ac17c84538
Mahdi-Alaee <mahdi.alaee.dev@gmail.com>
2024-02-27 19:26:15 +0330

    project 12 : using classTransformer library

--- practice12_third-party-libraries/src/app.ts
+++ practice12_third-party-libraries/src/app.ts
@@ -1,9 +1,27 @@
-import lodash from "lodash";
+// import lodash from "lodash";
 
-declare let Global: string;
+// declare let Global: string;
 
-const numbers = [1, 2, 3];
+// const numbers = [1, 2, 3];
 
-console.log(lodash.shuffle(numbers));
-console.log(Global);
+// console.log(lodash.shuffle(numbers));
+// console.log(Global);
+
+//* class transformer *//
+
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { Product } from "./product";
+
+const products = [
+  { title: "laptop", price: 499.99 },
+  { title: "phone", price: 299.99 },
+];
+
+const plainedProducts = plainToClass(Product, products);
+
+for(let prd of plainedProducts){
+    console.log(prd.getInformation());
+}
+
 
